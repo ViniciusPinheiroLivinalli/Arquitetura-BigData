e31accc47637c4be67b260ee2f9dde60589977fd
Vinicius Pinheiro Livinalli <167373193+ViniciusPinheiroLivinalli@users.noreply.github.com>
2026-04-12 14:22:23 -0300

    feat logs

--- vistas_lote/acessos_por_pais.py
+++ vistas_lote/acessos_por_pais.py
@@ -25,3 +25,22 @@ vista = df_total.groupby("country")["accessed_date"].agg(
 pasta_saida = pasta_raiz / "vistas_lote"
 os.makedirs(pasta_saida, exist_ok=True)
 vista.to_csv(pasta_saida / "acessos_por_pais.csv", index=False)
+
+pasta_gerenciamento = pasta_raiz / "gerenciamento"
+os.makedirs(pasta_gerenciamento, exist_ok=True)
+
+# LOG DE PROCESSAMENTO
+log_path = pasta_gerenciamento / "log_processamento.csv"
+
+nova_entrada = pd.DataFrame([{
+    "arquivo": str(pasta_saida / "acessos_por_pais.csv"),
+    "timestamp": pd.Timestamp.now(),
+    "qnt_linhas": len(vista),
+    "status": "Processado"
+}])
+
+# Se o log já existe, adiciona ao final. Se não, cria.
+if log_path.exists():
+    nova_entrada.to_csv(log_path, mode="a", header=False, index=False)
+else:
+    nova_entrada.to_csv(log_path, index=False)
--- vistas_lote/vendas_por_metodo_pagamento.py
+++ vistas_lote/vendas_por_metodo_pagamento.py
@@ -26,3 +26,20 @@ vista = df_total.groupby("pay_method")["sales"].agg(
 pasta_saida = pasta_raiz / "vistas_lote"
 os.makedirs(pasta_saida, exist_ok=True)
 vista.to_csv(pasta_saida / "vendas_por_metodo_pagamento.csv", index=False)
+
+# Log de processamento
+pasta_gerenciamento = pasta_raiz / "gerenciamento"
+os.makedirs(pasta_gerenciamento, exist_ok=True)
+log_path = pasta_gerenciamento / "log_processamento.csv"
+nova_entrada = pd.DataFrame([{
+    "arquivo": str(pasta_saida / "vendas_por_metodo_pagamento.csv"),
+    "timestamp": pd.Timestamp.now(),
+    "qnt_linhas": len(vista),
+    "status": "Processado"
+}])
+
+# Se o log já existe, adiciona ao final. Se não, cria.
+if log_path.exists():
+    nova_entrada.to_csv(log_path, mode="a", header=False, index=False)
+else:
+    nova_entrada.to_csv(log_path, index=False)
